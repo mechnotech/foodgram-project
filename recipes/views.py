eea6f2b7e2f20fc6ed989a9b0c9096c71201ff33
Evgenii Shumilov <mechnotech@yandex.ru>
2020-11-27 14:11:36 +0300

    templates edit

--- recipes/views.py
+++ recipes/views.py
@@ -2,8 +2,9 @@ from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Recipe
 
-def get_paginated_view(request, post_list, page_size=10):
-    paginator = Paginator(post_list, page_size)
+
+def get_paginated_view(request, recipe_list, page_size=6):
+    paginator = Paginator(recipe_list, page_size)
     page_number = request.GET.get('page')
     page = paginator.get_page(page_number)
     return page, paginator
