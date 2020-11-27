d451a917146b7dc7f820d5f0553001c7e751d423
Evgenii Shumilov <mechnotech@yandex.ru>
2020-11-27 14:30:23 +0300

    users app created

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -12,13 +12,14 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
+    'users',
+    'recipes',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'recipes',
 ]
 
 MIDDLEWARE = [
