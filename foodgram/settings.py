91aa2ba3059c3b628bbe3760106b74f303ee53a1
Evgenii Shumilov <mechnotech@yandex.ru>
2020-11-27 15:21:47 +0300

    users can signup and login

--- foodgram/settings.py
+++ foodgram/settings.py
@@ -22,6 +22,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 ]
 
+LOGIN_REDIRECT_URL = 'index'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
