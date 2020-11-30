8b011efe741f2a0803f51e6baa5ab7ad91373fab
Evgenii Shumilov <mechnotech@yandex.ru>
2020-11-30 09:54:37 +0300

    pagination complete

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,6 @@
 from django.core.paginator import Paginator
-from django.shortcuts import render
-from .models import Recipe
+from django.shortcuts import render, get_object_or_404
+from .models import Recipe, User
 
 
 def get_paginated_view(request, recipe_list, page_size=6):
@@ -15,3 +15,12 @@ def index(request):
     page, paginator = get_paginated_view(request, recipe_list)
     return render(request, 'indexNotAuth.html',
                   {'page': page, 'paginator': paginator})
+
+
+def profile(request, username):
+    author = get_object_or_404(User, username=username)
+    recipe_list = author.recipe.all()
+    page, paginator = get_paginated_view(request, recipe_list)
+    return render(request, 'authorRecipe.html',
+                  {'page': page, 'paginator': paginator,
+                   'author': author})
